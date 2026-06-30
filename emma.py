4b8fd5300ca23cc295dcb4b83de400ecd3514635
Emmie Yung <isaiahemmanuel129@gmail.com>
2026-06-30 17:11:02 +0100

    python code

---
+++ emma.py
@@ -0,0 +1,69 @@
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_core.messages import AIMessage, HumanMessage, SystemMessage
+
+from flask import Flask, render_template, request, jsonify
+
+
+app = Flask(__name__)
+
+load_dotenv()
+
+model = ChatGoogleGenerativeAI(model="gemini-2.5-flash")
+
+prompt=[
+
+@app.route("/")
+def indexHome():
+    return render_template("index.html")
+
+@app.route("/", methods=["POST"])
+def index():
+    
+    chats=[
+        SystemMessage(content="You are a care center agent assistant")
+    ]
+    history = []
+    
+    while True:
+        user = input("You😍: ")
+        humanMessage = HumanMessage(content=user)
+        chats.append(humanMessage)
+        history.append({"user":user})
+        if user == "exit":
+            break
+        res = model.invoke(chats)
+        chats.append(AIMessage(content=res.content))
+        history.append({"result": res.content})
+        return jsonify({"data": history})
+
+
+
+ 
+chats=[
+    SystemMessage(content="You are a care center agent assistant")
+]
+history = []
+
+while True:
+    user = input("You😍: ")
+    humanMessage = HumanMessage(content=user)
+    chats.append(humanMessage)
+
+    history.append({"user":user})
+    if user == "exit":
+        break
+    res = model.invoke(chats)
+    chats.append(AIMessage(content=res.content))
+    history.append({"result": res.content})
+    print("result: ", res.content)
+
+# for i in chats:
+#     i[]
+   
+print(history)
+
+res = model.invoke(prompt)
+
+print(res.content)
+if __name__ == "__main__":
+    app.run(debug=True)
